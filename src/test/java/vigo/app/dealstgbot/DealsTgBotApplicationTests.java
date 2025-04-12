2eb701d62e5c74f45eab01c620556485d3014144
vigo1113 <viacheslav.gorbunov@NetCracker.com>
2025-04-13 00:37:05 +0300

    Добавлены файлы конфигурации Maven и начальная структура проекта для Telegram-бота. Созданы файлы .gitattributes, .gitignore, pom.xml, а также скрипты для запуска Maven (mvnw и mvnw.cmd). Добавлены ресурсы и тесты для приложения.

---
+++ src/test/java/vigo/app/dealstgbot/DealsTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package vigo.app.dealstgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
